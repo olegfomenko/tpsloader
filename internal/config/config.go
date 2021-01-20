b19d5f7c81c765417ffb49d460c3b1290993e938
olegfomenko <oleg.fomenko2002@gmail.com>
2021-01-20 03:39:12 +0200

    Adding configuration file

---
+++ internal/config/config.go
@@ -0,0 +1,6 @@
+package config
+
+type Config struct {
+	AdminSeed  string `yaml:"admin"`
+	HorizonURL string `yaml:"horizon"`
+}
--- internal/main/main.go
+++ internal/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/olegfomenko/tpsloader/internal/config"
 	"github.com/olegfomenko/tpsloader/internal/operations"
 	"github.com/stellar/go/clients/horizonclient"
 	"github.com/stellar/go/keypair"
@@ -8,14 +9,20 @@ import (
 )
 
 func main() {
+	// reading config file
+	conf, err := config.NewConfig("config.yml")
+	if err != nil {
+		panic(err)
+	}
+
 	// Getting admin keypair using secret key
-	admin, _ := keypair.ParseFull("SDR7XY33FYTDJTRF2CAXU5VGQWIQU4YOGZYPMYZ7ZAZTDGINQYMRJWZC")
+	admin, _ := keypair.ParseFull(conf.AdminSeed)
 	log.Println("Seed admin:", admin.Seed())
 	log.Println("Address admin:", admin.Address())
 
 	// Get horizon service API instance
 	client := horizonclient.DefaultTestNetClient
-	client.HorizonURL = "http://localhost:8000/"
+	client.HorizonURL = conf.HorizonURL
 
 	// Creating new account
 	account := operations.CreateAccount(admin, client)
